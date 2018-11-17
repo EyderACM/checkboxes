2a69e80e1cdecb6645344a7a616a014b90c25d6a
Eyder ACM <eyderacm@gmail.com>
2018-11-17 10:29:41 -0600

    documented code

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ let checks = document.querySelectorAll('input[type="checkbox"]');
 
 let lastChecked;
 
+//We search for every checkbox, and when clicked, we add the class "checked"
 checks.forEach(check => check.onclick = function(e){
 
 	let inBetween = false;
+	//if shift is pressed, and another box is checked, all in between will be checked
 	if(e.shiftKey && this.checked){
+		//We iterate through all checkboxes to see if any is checked
 		checks.forEach(checkd => {		
 			console.log(checkd);	
+			/*When we get to the first one that we clicked, or this one, we set "InBetween" to true, to say that we are now
+			iterating through the ones in the middle of the checked ones*/
 			if(checkd === this || checkd === lastChecked){
 				inBetween = !inBetween;							
 			}
-			
+			//If we are now iterating through the ones in between, we add the class checked to them
 			if(inBetween) {
 				checkd.checked = true;
 			}
